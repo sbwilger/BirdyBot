1eb4aa54c5423e462efd46fae569db686204cb76
Serana Wilger <sbwilger@gmail.com>
2020-06-22 12:16:46 -0400

    Add files via upload
    Added the ability to restrict commands to channels, but haven't used it quite yet.

---
+++ Attributes/ChannelCheckMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Serana Wilger
+/// 06/22/2020
+/// ChannelCheckMode.cs
+/// 
+/// The enum for setting the mode ofchecking if a command is allowed in a given channel
+/// </summary>
+
+namespace BirdyBot.Attributes
+{
+    public enum ChannelCheckMode
+    {
+        Any = 0,
+        None = 1,
+        MineOrParentAny = 2
+    }
+}
---
+++ Attributes/RequireCategoryAttribute.cs
@@ -0,0 +1,50 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Serana Wilger
+/// 06/22/2020
+/// RequireCategoryAttribute.cs
+/// 
+/// Limits commands to certain categories, or keeps them out.
+/// </summary>
+
+namespace BirdyBot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    public class RequireCategoryAttribute : CheckBaseAttribute
+    {
+        public IReadOnlyList<string> CategoryNames { get; }
+        public ChannelCheckMode CheckMode { get; }
+        public RequireCategoryAttribute(ChannelCheckMode checkMode, params string[] channelNames)
+        {
+            CheckMode = checkMode;
+            CategoryNames = new ReadOnlyCollection<string>(channelNames);
+        }
+
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            if(ctx.Guild == null || ctx.Member == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            bool contains = CategoryNames.Contains(ctx.Channel.Parent.Name, StringComparer.OrdinalIgnoreCase);
+
+            return CheckMode switch
+            {
+                ChannelCheckMode.Any => Task.FromResult(contains),
+
+                ChannelCheckMode.None => Task.FromResult(!contains),
+
+                _ => Task.FromResult(false)
+            };
+        }
+    }
+}
