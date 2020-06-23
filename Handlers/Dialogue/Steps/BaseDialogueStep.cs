24ec28b0e4478f79be530009402d0955b53cb7dd
Serana Wilger <sbwilger@gmail.com>
2020-06-23 12:47:36 -0400

    Add files via upload
    Added the steps folder

---
+++ Handlers/Dialogue/Steps/BaseDialogueStep.cs
@@ -0,0 +1,50 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Serana Wilger
+/// 06/23/2020
+/// BaseDialogueStep.cs
+/// 
+/// Abstract class for handling dialogue steps.
+/// </summary>
+
+namespace BirdyBot.Handlers.Dialogue.Steps
+{
+    public abstract class BaseDialogueStep : IDialogueStep
+    {
+        protected readonly string _content;
+
+        // constructor
+        public BaseDialogueStep(string content)
+        {
+            _content = content;
+        }
+
+        public Action<DiscordMessage> OnMessageAdded { get; set; } = delegate{};
+
+        public abstract IDialogueStep NextStep { get; }
+
+        public abstract Task<bool> ProcessStep(DiscordClient client, DiscordChannel channel, DiscordUser user);
+
+        //sends a "Try Again" message when something goes wrong
+        protected async Task TryAgain(DiscordChannel channel, string problem)
+        {
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder
+            {
+                Title = "Please Try Again",
+                Color = DiscordColor.Red
+            };
+
+            embedBuilder.AddField("There was a problem with your previous input", problem);
+
+            DiscordMessage embed = await channel.SendMessageAsync(embed: embedBuilder).ConfigureAwait(false);
+
+            OnMessageAdded(embed);
+        }
+    }
+}
