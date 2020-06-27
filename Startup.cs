fd25b64646312a3a77a976db6bff6d2803d8f759
Serana Wilger <sbwilger@gmail.com>
2020-06-27 07:09:46 -0400

    Add files via upload
    Startup file for the bot

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Serana Wilger
+/// 06/27/2020
+/// Startup.cs
+/// 
+/// Startup file for the bot
+/// </summary>
+
+namespace sbwilger.BirdyBot
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            Bot bot = new Bot(serviceProvider);
+            services.AddSingleton(bot);
+        }
+
+        public void Configure(IApplicationBuilder abb, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
