d6ec9b1f77d59de968b4f4e05a6ccc420dba354e
Serana Wilger <sbwilger@gmail.com>
2020-07-05 19:32:16 -0400

    Add files via upload

---
+++ Commands/ProfileCommands.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using sbwilger.Core.Services.Profiles;
+using sbwilger.DAL.Models.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sbwilger.BirdyBot.Commands
+{
+    public class ProfileCommands : BaseCommandModule
+    {
+        private readonly IProfileService _profileService;
+
+        public ProfileCommands(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+
+        [Command("profile")]
+        public async Task Profile(CommandContext ctx)
+        {
+            await GetProfileToDisplayAsync(ctx, ctx.Member.Id);
+        }
+
+        [Command("profile")]
+        public async Task Profile(CommandContext ctx, DiscordMember member)
+        {
+            await GetProfileToDisplayAsync(ctx, member.Id);
+        }
+
+        private async Task GetProfileToDisplayAsync(CommandContext ctx, ulong memberID)
+        {
+            Profile profile = await _profileService.GetOrCreateProfileAsync(memberID, ctx.Guild.Id).ConfigureAwait(false);
+
+            DiscordMember member = ctx.Guild.Members[profile.DiscordId];
+
+            DiscordEmbedBuilder profileEmbed = new DiscordEmbedBuilder
+            {
+                Title = $"{member.DisplayName}'s Profile",
+                Color = DiscordColor.Cyan
+            };
+
+            profileEmbed.WithThumbnail(member.AvatarUrl);
+            profileEmbed.AddField("Xp", profile.Exp.ToString());
+
+            await ctx.Channel.SendMessageAsync(embed: profileEmbed).ConfigureAwait(false);
+        }
+    }
+}
